657f5a06a6ed8c2e2ccb92c1b3ac31e7c45bbf1d
Brad Davidson <65302404+Brad-Davidson@users.noreply.github.com>
2021-10-14 16:11:19 -0400

    added html files + navigation

--- src/main/java/com/uc/BoredNoMore/controllers/MainController.java
+++ src/main/java/com/uc/BoredNoMore/controllers/MainController.java
@@ -1,5 +1,54 @@
 package com.uc.BoredNoMore.controllers;
 
-public class MainController {
+import javax.annotation.PostConstruct;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@PostConstruct
+	private void loadData() {
+		//this is where we would potentially get auth information in the future.
+		
+		//with the user information, we can pull in data regarding todo list and custom activities
+	}
+	
+	/**
+	 * 
+	 * @return the home screen template
+	 */
+	@GetMapping("/")
+	public String getHome() {
+		return "index";
+	}
+	
+	/**
+	 * 
+	 * @return The template for finding activities
+	 */
+	@GetMapping("/activity")
+	public String getActivities() {
+		return "activities";
+	}
+	
+	/**
+	 * 
+	 * @return The template for adding custom activities
+	 */
+	@GetMapping("/addactivity")
+	public String getAddActivity() {
+		return "addactivity";
+	}
+	
+	/**
+	 * 
+	 * @return The template for viewing and managing the Todo list
+	 */
+	@GetMapping("/todo")
+	public String getTodo() {
+		return "todo";
+	}
+	
 }
