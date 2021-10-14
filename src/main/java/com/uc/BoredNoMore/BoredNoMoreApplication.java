1ccb8cdac18853b03d5fb1bd2c5ca6dd89790afb
Brad Davidson <65302404+Brad-Davidson@users.noreply.github.com>
2021-10-14 13:58:30 -0400

    initial commit

---
+++ src/main/java/com/uc/BoredNoMore/BoredNoMoreApplication.java
@@ -0,0 +1,13 @@
+package com.uc.BoredNoMore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoredNoMoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoredNoMoreApplication.class, args);
+	}
+
+}
