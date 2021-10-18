6b97456cff7f24aafc2cf89d91434454d9518b20
Xavier Baszuk <48223045+baszukxj@users.noreply.github.com>
2021-10-18 01:42:47 -0400

    Create IActivityDAO.java

---
+++ src/main/java/com/uc/BoredNoMore/dao/IActivityDAO.java
@@ -0,0 +1,33 @@
+package com.uc.BoredNoMore.dao;
+
+import com.uc.BoredNoMore.dto.ActivityDTO;
+import com.uc.BoredNoMore.dto.TodoDTO;
+
+/**
+ * Add,Delete, and Get activity lists from database calls here
+ * @author Xavier Baszuk
+ *
+ */
+public interface IActivityDAO {
+
+	/**
+	 * Add activity to database
+	 * @return void
+	 */s
+	void addActivity(ActivityDTO activity);
+	
+	/**
+	 * Delete activity from database
+	 * @return void
+	 */
+	void deleteActivity(ActivityDTO activity);
+	
+	/**
+	 * Get activity from database with list
+	 * @return ActivityDTO list
+	 */
+	ActivityDTO getActivities(TodoDTO todoList);
+	
+	
+}
+
